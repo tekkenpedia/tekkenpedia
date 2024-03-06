3bbfe6e0c7669086225291b0c099f800be564d84
Steevan BARBOYON <steevan.barboyon@gmail.com>
2024-03-06 17:27:23 +0100

    Add Convert and Concat commands

---
+++ src/Ffmpeg/Filter/Gif30FpsFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Ffmpeg\Filter;
+
+use FFMpeg\Filters\Gif\GifFilterInterface;
+use FFMpeg\Media\Gif;
+
+class Gif30FpsFilter implements GifFilterInterface
+{
+    public function getPriority(): int
+    {
+        return 1;
+    }
+
+    public function apply(Gif $gif): array
+    {
+        return ['-r', '30'];
+    }
+}
