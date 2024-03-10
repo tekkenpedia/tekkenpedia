76639e3dee474c41025ee5ca9b25dc0d5fff092b
Steevan BARBOYON <steevan.barboyon@gmail.com>
2024-03-10 16:45:38 +0100

    Add toggler

--- src/Move/Section.php
+++ src/Move/Section.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Move;
 
 use App\Collection\Move\MoveCollection;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 readonly class Section
 {
+    public string $slug;
+
     public function __construct(public string $name, public MoveCollection $moves)
     {
+        $this->slug = (new AsciiSlugger())->slug($this->name)->toString();
     }
 }
