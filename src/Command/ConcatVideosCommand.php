f3dcb5dc523b253bf291979a2444368bbd3dc1a2
Steevan BARBOYON <steevan.barboyon@gmail.com>
2024-06-18 21:18:29 +0200

    Rework du répertoire data/

--- src/Command/ConcatVideosCommand.php
+++ src/Command/ConcatVideosCommand.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\{
     Input\InputInterface,
     Output\OutputInterface
 };
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class ConcatVideosCommand extends Command
 {
@@ -30,6 +32,32 @@ class ConcatVideosCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $dir = __DIR__ . '/../../data/characters';
+        $filesystem = new Filesystem();
+        $files = (new Finder())->files()->in($dir);
+        foreach ($files as $file) {
+            $movesDir = $file->getPath() . '/moves';
+//            $filesystem->mkdir($movesDir);
+
+            $json = json_decode(file_get_contents($file->getPathname()), true, 512, JSON_THROW_ON_ERROR);
+
+            foreach ($json['sections'] ?? [] as $section) {
+                foreach ($section['moves'] ?? [] as $id => $move) {
+                    $move = array_merge(['id' => $id], $move);
+                    file_put_contents($movesDir . '/' . $id . '.json', json_encode($move, JSON_PRETTY_PRINT) . "\n");
+                }
+
+                foreach ($section['sections'] ?? [] as $subSection) {
+                    foreach ($subSection['moves'] ?? [] as $id => $move) {
+                        $move = array_merge(['id' => $id], $move);
+                        file_put_contents($movesDir . '/' . $id . '.json', json_encode($move, JSON_PRETTY_PRINT) . "\n");
+                    }
+                }
+            }
+        }
+
+        die;
+
         $this->concat->concat($output);
 
         return static::SUCCESS;
