649b9e6bcf9d3ce611678d16764b2b21a5fc955f
Steevan BARBOYON <steevan.barboyon@gmail.com>
2025-12-20 13:17:58 +0100

    Add videos for Victor, sort defenses

--- src/Collection/Character/Move/Attack/DefenseEnumCollection.php
+++ src/Collection/Character/Move/Attack/DefenseEnumCollection.php
@@ -25,4 +25,45 @@ class DefenseEnumCollection extends AbstractObjectCollection
 
         return $return;
     }
+
+    public function set(int|string $key, mixed $value): static
+    {
+        parent::set($key, $value);
+        $this->sort();
+
+        return $this;
+    }
+
+    public function add(mixed $value): static
+    {
+        parent::add($value);
+        $this->sort();
+
+        return $this;
+    }
+
+    public function sort(): static
+    {
+        $sorted = [
+            DefenseEnum::SSL,
+            DefenseEnum::SWL,
+            DefenseEnum::SSR,
+            DefenseEnum::SWR,
+            DefenseEnum::DUCK,
+            DefenseEnum::REVERSAL,
+            DefenseEnum::POWER_CRUSH,
+            DefenseEnum::LOW_PARRY,
+            DefenseEnum::PUNISH,
+            DefenseEnum::THROW,
+        ];
+
+        usort(
+            $this->values,
+            function (DefenseEnum $defenseA, DefenseEnum $defenseB) use ($sorted) {
+                return array_search($defenseA, $sorted, true) <=> array_search($defenseB, $sorted, true);
+            }
+        );
+
+        return $this;
+    }
 }
