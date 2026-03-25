7ae8d5009c0529ceb028c7ac6942a7830ffcc176
Steevan BARBOYON <steevan.barboyon@gmail.com>
2026-03-25 16:49:58 +0100

    Ajout de la recherche

--- docs/scripts/moves.js
+++ docs/scripts/moves.js
@@ -43,6 +43,51 @@ $(function() {
         });
     });
 
+    $('[data-filter-used]').on('click', function() {
+        let $btn = $(this);
+        let filter = $btn.data('filter-used');
+
+        $('[data-filter-used]').removeClass('active');
+        $btn.addClass('active');
+
+        $('[data-used]').each(function() {
+            let $move = $(this);
+            if (filter === 'all' || $move.data('used') === filter) {
+                $move.show();
+            } else {
+                $move.hide();
+            }
+        });
+
+        // Show all sections first to reset state
+        $('h1[data-toggle-id], h2[data-toggle-id]').show();
+        $('h1[data-toggle-id], h2[data-toggle-id]').each(function() {
+            let id = $(this).data('toggle-id');
+            $('div[data-toggle=' + id + ']').show();
+        });
+
+        // Hide sections (h2 then h1) that have no visible moves
+        $('h2[data-toggle-id]').each(function() {
+            let $header = $(this);
+            let id = $header.data('toggle-id');
+            let $content = $('div[data-toggle=' + id + ']');
+            if ($content.find('[data-used]:visible').length === 0) {
+                $header.hide();
+                $content.hide();
+            }
+        });
+
+        $('h1[data-toggle-id]').each(function() {
+            let $header = $(this);
+            let id = $header.data('toggle-id');
+            let $content = $('div[data-toggle=' + id + ']');
+            if ($content.find('[data-used]:visible').length === 0) {
+                $header.hide();
+                $content.hide();
+            }
+        });
+    });
+
     function copyToClipboard(toCopy, confirmationMessage, event) {
         navigator.clipboard.writeText(toCopy);
 
